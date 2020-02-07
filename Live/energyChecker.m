bb2db6a213f6c3c87f61e9bf4f5454bf376ec269
petemik <petemikhaeil3@gmail.com>
2020-02-07 13:37:31 +0000

    Checks Energy

---
+++ Live/energyChecker.m
@@ -0,0 +1,77 @@
+% Define constants
+G = 6.674e-11;
+year = 3.154e7;
+millenia = 1000*year;
+
+%planets initial conditions [x0;y0;vx0;vy0] & masses
+sun =       [0;         0;  0;  0];
+mercury =   [54e9;      0;  0;  47.4e3];
+venus =     [108e9;     0;  0;  35.3e3];
+earth =     [147e9;     0;  0;  30.3e3];
+mars =      [206e9;     0;  0;  26.5e3];
+jupiter =   [740e9;     0;  0;  13.1e3];
+saturn =    [135e10;    0;  0;  9.7e3];
+uranus =    [2.7e12;    0;  0;  6.8e3];
+neptune =   [4.5e12;    0;  0;  5.4e3];
+masses = [  2e30; 
+            0.33e24;
+            4.87e24;
+            5.97e24;
+            0.642e24;
+            1898e24;
+            568e24;
+            86.7e24;
+           102e24];
+initial = [ sun;
+            mercury; 
+            venus; 
+            earth; 
+            mars; 
+            jupiter; 
+            saturn; 
+            uranus; 
+            neptune];
+
+%Perform in-built Runge-Kutta
+func = @(t, y) multiEqs(t, y, masses);
+end_time = 10*year;
+tspan = [0 end_time];
+stepsize = 1/5000*year;
+[t,y] = rungeKutta(func,tspan, initial, stepsize);
+total_steps = ceil((tspan(2)-tspan(1))/stepsize);
+%NOT SURE WHY WE NEED +1 BUT APPARANTLY WE DO
+n_bodies = size(masses, 1);
+T = zeros(n_bodies, total_steps+1);
+% Calculate the Kinetic Energy of each planet
+for i=0:n_bodies-1
+    T(i+1, :) = 1/2*masses(i+1)*(y(4*i+3, :).^2 + y(4*i+4, :).^2);
+end
+% Sum to a total kinetic energy of the system
+total_T = sum(T);
+
+%Calculate potential energy, this is gonna be long
+% Loop round this and do it for every element of the sytem maybe who tf
+% knows
+V = zeros(n_bodies, total_steps+1);
+
+% First lets just do potential energy of the Sun
+for j=1:n_bodies
+    for i=1:n_bodies
+        if i~=j
+            V(i, :) = V + -G*masses(j)*masses(i)*...
+                ((y(1, :)- y(4*(i-1)+1, :)).^2 +(y(2, :)- y(4*(i-1)+2, :)).^2).^(-1/2);
+        end
+    end
+end
+    
+
+
+% 
+% %Plot orbits 
+% hold on
+% n_bodies = size(masses, 1);
+% for i=0:n_bodies-1
+%     plot(y(4*i+1,:), y(4*i+2, :))
+% end
+% legend('Sun', 'Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune')
+% hold off
