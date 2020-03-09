4a98403837bf5471622947f28c13b6242d7986c5
murraymccreery <57140450+murraymccreery@users.noreply.github.com>
2020-03-09 11:48:51 +0000

    IC change & CoM adapt

--- Live/selfImplementRun.m
+++ Live/selfImplementRun.m
@@ -3,15 +3,15 @@ year = 3.154e7;
 millenia = 1000*year;
 
 %planets initial conditions [x0;y0;vx0;vy0] & masses
-sun =       [0;         0;  0;  0];
-mercury =   [46e9;      0;  0;  47.4e3];
-venus =     [108e9;     0;  0;  35.3e3];
-earth =     [147e9;     0;  0;  30.3e3];
-mars =      [206e9;     0;  0;  26.5e3];
-jupiter =   [740e9;     0;  0;  13.1e3];
-saturn =    [135e10;    0;  0;  9.7e3];
-uranus =    [2.7e12;    0;  0;  6.8e3];
-neptune =   [4.5e12;    0;  0;  5.4e3];
+sun =       [-6.4382245E+08;	1.0921744E+09;	-1.4241571E+01;	-4.4343761E+00];
+mercury =   [-5.7995526E+10;	2.8370562E+09;	-1.1628598E+04;	-4.6603509E+04];
+venus =     [-2.3107878E+10;	1.0625732E+11;	-3.4381530E+04;	-7.5130530E+03];
+earth =     [-1.4132709E+11;	4.7898844E+10;	-9.8949287E+03;	-2.8387273E+04];
+mars =      [-9.8203828E+10;	-2.0338091E+11;	2.2767800E+04;	-8.3582128E+03];
+jupiter =   [1.4542722E+11;     -7.6379101E+11;	1.2675763E+04;	3.0667018E+03];
+saturn =    [6.1148506E+11;     -1.3685160E+12;	8.2848034E+03;	3.9134473E+03];
+uranus =    [2.4057104E+12;     1.7310000E+12;	-4.0273876E+03;	5.2104067E+03];
+neptune =   [4.3799648E+12; 	-9.2323714E+11;	1.0847279E+03;	5.3513090E+03];
 masses =    [2e30; 
             0.33e24;
             4.87e24;
@@ -34,11 +34,14 @@ initial = [ sun;
 tic
 %Perform in-built Runge-Kutta
 func = @(t, y) multiEqs(t, y, masses);
-end_time = 10*year;
+end_time = millenia;
 tspan = [0 end_time];
 stepsize = (1/1000)*year;
-[t,y] = rungeKuttaFehlberg56(func, tspan, initial, stepsize,1e-6);
+%[t,y] = rungeKuttaFehlberg56(func, tspan, initial, stepsize,1e-6);
 %[t,y] = rungeKutta(func, tspan, initial, stepsize);
+opts = odeset('RelTol',1e-2,'AbsTol',1e-4, 'MaxStep', stepsize);
+[t,y] = ode45(func,tspan, initial, opts);
+y = transpose(y);
 toc
 
 % fileID = fopen('test-data.txt','w');
@@ -50,7 +53,7 @@ toc
 hold on
 n_bodies = size(masses, 1);
 for i=0:n_bodies-1
-    plot(y(4*i+1,:), y(4*i+2, :))
+    plot(y(4*i+1,:), y(4*i+2,:))
 end
 legend('Sun', 'Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune')
 hold off
